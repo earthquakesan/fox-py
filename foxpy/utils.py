86286d3e47b13a6c5472a2c1dfcd4e33080254da
Ivan Ermilov <earthquakesan@gmail.com>
2017-08-09 14:25:16 +0200

    fix bug when there are no nif phrases

--- foxpy/utils.py
+++ foxpy/utils.py
@@ -1,8 +1,11 @@
 """Utils for foxpy."""
 
 def extractNifPhrases(json_ld):
-    graph = json_ld["@graph"]
-    return list(filter(lambda x: x["@type"] == "nif:Phrase", graph))
+    if "@graph" in json_ld:
+        graph = json_ld["@graph"]
+        return list(filter(lambda x: x["@type"] == "nif:Phrase", graph))
+    else:
+        return []
 
 def insertCharacterAtPosition(string, char, position):
     return string[:position] + char + string[position:]
